6b0daf3dfa4b2a1d8cfea86e4c35b2fb4f5f3133
Johannes Reker <info@jreker.de>
2024-11-26 17:39:54 +0100

    fix: added debug outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ls -la
+echo ${all_args} 
 set -e
 all_args=$@
-/usr/share/dependency-check/bin/dependency-check.sh  ${all_args} 
+/usr/share/dependency-check/bin/dependency-check.sh ${all_args} 
